39a61f9604342e844011c064a56cf2506f6a0910
mouseroot <mouseroot@gmail.com>
2024-03-21 07:51:06 -0500

    Adjusted sniffer.py to read Ethernet Packets

--- sniffer.py
+++ sniffer.py
@@ -3,27 +3,27 @@
 import socket
 from struct import *
 
-raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+raw_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 
+#Convert a string of 6 characters of ethernet address into a dash separated hex string
+def eth_addr (a):
+    a = [ord(str(x)) for x in a]
+    try:
+        b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (a)
+        return b
+    except TypeError:
+        print(f"Error Formating Address: {a}")
+        input("Paused...Enter to resume!")
+        return ""
+        
 
 while 1:
     packet = raw_socket.recvfrom(65565)
     packet = packet[0]
 
-    ip_header = packet[0:20]
-
-    iph = unpack("!BBHHHBBH4s4s", ip_header)
-
-    version = iph[0]
-    version = version >> 4
-
-    header_length = version & 0xF
-    header_length *= 4
-
-    ttl = iph[5]
-    protocol = iph[6]
-    if protocol == 6:
-        protocol = "TCP"
-    s_addr = socket.inet_ntoa(iph[8]);
-    d_addr = socket.inet_ntoa(iph[9]);
-    print(f"Version {version} - Length: {header_length} - TTL: {ttl} - PROTOCAL: {protocol} \n{s_addr} <---> {d_addr}")
+    ethernet_length = 14
+    ethernet_header = packet[:ethernet_length]
+    eth = unpack('!6s6sH' , ethernet_header)
+    eth_protocol = socket.ntohs(eth[2])
+    #print(f"Dest MAC : {packet[0:6]}")
+    print(f'Destination MAC : { eth_addr(packet[0:6]) }  Source MAC :  { eth_addr(packet[6:12]) } Protocol : { str(eth_protocol) }')
